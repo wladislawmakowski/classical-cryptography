c1c73710672ec0f30eb54475e0e9b789b055300f
ant-oi <marieantoinette98775420@yahoo.com>
2020-10-19 00:33:49 +0600

    ordinary commit

--- source/implementations/vigenere.c
+++ source/implementations/vigenere.c
@@ -1,78 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
-	int mod(int n, int d);
-	char _alpha[75] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+";
+static char _alpha[89] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=~!@#$%^&*()_+{};':,./<>? ";
+static int k1, k2;
 
-	int i, j;
-	int k1, k2;
-	char m[1024];
-	char k[1024];
-	char c[1024];
-
-	scanf("%s", m);
-	scanf("%s", k);
-
-	for(i = 0;i < 1024;i++)
-	{
-		for(j = 0;j < 75;j++)
-		{
-			if(m[i] == _alpha[j])
-			{
-				k1 = j;
-			}
-
-			if(k[i] == _alpha[j])
-			{
-				k2 = j;
-			}
-		}
-
-		c[i] = _alpha[(k1 + k2)%75];
-	}
-
-	for(i = 0;i < 1024;i++)
-	{
-		printf("%c", c[i]);
-	}
-	printf("\n");
-
-	printf("-----------------------\n");
-
-	for(i = 0;i < 1024;i++)
-	{
-		for(j = 0;j < 75;j++)
-		{
-			if(c[i] == _alpha[j])
-			{
-				k1 = j;
-			}
-
-			if(k[i] == _alpha[j])
-			{
-				k2 = j;
-			}
-		}
-
-		printf("|i: %d||%d = %c|\n", i, ((k1-k2)%75), _alpha[mod((k1 - k2), 75)]);
-		c[i] = _alpha[mod((k1 - k2), 75)];
-	}
-
-	printf("------------------------\n");
-
-	for(i = 0;i < 1024;i++)
-	{
-		printf("%c", c[i]);
-	}
-	printf("\n");
-
-	printf("%d\n", mod(-42, 75));
-
-	return 0;
-}
-
-int mod(int n, int d)
+static int mod(int n, int d)
 {
     int result = n % d;
     if (result * d < 0)
@@ -80,4 +11,40 @@ int mod(int n, int d)
     return result;
 }
 
-//c[i] = (char)((((int)m[i] + (int)k[i])%26)+65);
+static char encrypt(
+	const int MODE,
+	const char key,
+	const char message
+)
+{
+	int i;
+	for(i = 0;i < 89; i++)
+	{
+		if(_alpha[i] == message)
+			k1 = i;
+		if(_alpha[i] == key)
+			k2 = i;
+	}
+
+	if(MODE == 1)
+		return _alpha[mod((k1 + k2), 89)];
+	else if(MODE == -1)
+		return _alpha[mod((k1 - k2), 89)];
+	return message;
+
+}
+
+void vigenere
+(
+	const int MODE,
+	char* const from,
+	char* const key,
+	char* to
+)
+{
+	char* p_from = from;
+	char* p_key = key;
+	for(;*p_from != '\0', *p_key != '\0'; ++p_from, ++p_key)
+		*to++ = encrypt(MODE, *p_key, *p_from);
+	*to = '\0';
+}
